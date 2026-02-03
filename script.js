372ff61786668725700a9867542f1b5c9d13e8d3
Vansh Saxena <167329440+VanshSaxena3@users.noreply.github.com>
2026-02-03 19:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
+  if (num === 0) return "";
+
+  const values = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   let result = "";
 
-  const special = [
-    ['CM', 900],
-    ['CD', 400],
-    ['XC', 90],
-    ['XL', 40],
-    ['IX', 9],
-    ['IV', 4]
-  ];
-
-  for (let i = 0; i < special.length; i++) {
-    while (num >= special[i][1]) {
-      result += special[i][0];
-      num -= special[i][1];
-    }
-  }
-
-  for (let key in obj) {
-    let symbol = obj[key][0];
-    let value = obj[key][1];
-
-    while (num >= value) {
-      result += symbol;
-      num -= value;
+  for (let i = 0; i < values.length; i++) {
+    while (num >= values[i][1]) {
+      result += values[i][0];
+      num -= values[i][1];
     }
   }
 
